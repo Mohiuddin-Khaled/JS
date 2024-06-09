12715c15edc88b9359dc5547232df5fae70854da
Mohiuddin-Khaled <119692593+Mohiuddin-Khaled@users.noreply.github.com>
2024-06-09 11:31:01 +0600

    Add files via upload

--- Practice_Problem_1(Module_5)/problem_6.js
+++ Practice_Problem_1(Module_5)/problem_6.js
@@ -1,10 +1,22 @@
 const prompt = require("prompt-sync")();
 // write js code here
 var n = parseInt(prompt("Enter the value:"));
-var numbers = [];
+var numbers = new Array(n);
 for (var i = 0; i < n; i++) {
     numbers[i] = parseInt(prompt());
 }
+
+// sorted array
+for (var x = 0; x < n - 1; x++) {
+    for (var y = x + 1; y < n; y++) {
+        if (numbers[x] > numbers[y]) {
+            var tmp = numbers[x];
+            numbers[x] = numbers[y];
+            numbers[y] = tmp;
+        }
+    }
+}
+
 var i, j, k;
 for (i = 0; i < n - 1; i++) {
     for (j = i + 1; j < n; j++) {
